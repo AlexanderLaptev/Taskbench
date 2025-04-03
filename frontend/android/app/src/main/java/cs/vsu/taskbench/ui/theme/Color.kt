8be4dc3cd411a31c8775bd051ee1226abd0e8c83
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-03 14:17:42 +0300

    set up project

---
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package cs.vsu.taskbench.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xFF444444)
+val LightGray = Color(0xFFAEAEAE)
+val AccentYellow = Color(0xFFFFE375)
+val Beige = Color(0xFFFFF3DC)
+val White = Color(0xFFFFFFFF)
+val LightYellow = Color(0xFFFFEBB4)
---
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/theme/Theme.kt
@@ -0,0 +1,11 @@
+package cs.vsu.taskbench.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskbenchTheme(
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(content = content)
+}
