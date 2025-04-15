97b1f995f0b620af0ace5516dd6edc23225b04e6
imbeer <alekceykocenko@gmail.com>
2025-04-15 11:43:16 +0300

    #9 fix database migration

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Applying database migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Starting Gunicorn..."
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 backend.wsgi:application
