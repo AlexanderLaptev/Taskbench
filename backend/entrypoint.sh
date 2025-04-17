d41fdcbb3c60c67c81ee25b6c0a7034f347ac1d3
imbeer <alekceykocenko@gmail.com>
2025-04-17 23:21:12 +0300

    #9 add simple healthcheck to taskbench-backend service

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@ echo "Applying database migrations..."
 #python manage.py makemigrations
 python manage.py migrate
 
+echo "Marking container as ready..."
+touch /tmp/app_ready
+
 echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8000 --workers 3 backend.wsgi:application
