f39854d090b4133e4396877a4c3b062556422bcd
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-22 06:17:01 +0300

    switch keyboard to password mode if text field is used for password input

--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/component/TextField.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/component/TextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +39,7 @@ fun TextField(
     placeholder: String = "",
     readOnly: Boolean = false,
     password: Boolean = false,
+    keyboardOptions: KeyboardOptions? = null,
     interactionSource: MutableInteractionSource? = null,
 ) {
     val shouldShowPlaceholder = value.isEmpty()
@@ -48,6 +51,9 @@ fun TextField(
         textStyle = textStyle,
         readOnly = readOnly,
         singleLine = true,
+        keyboardOptions = keyboardOptions ?: KeyboardOptions(
+            keyboardType = if (password) KeyboardType.Password else KeyboardType.Text
+        ),
 
         visualTransformation = if (password) {
             PasswordVisualTransformation()
