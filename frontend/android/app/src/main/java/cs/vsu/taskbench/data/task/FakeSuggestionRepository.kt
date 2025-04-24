b5f1254bd63c19c31fb895e62b09c420fdb72028
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-25 02:25:23 +0300

    #39: improve fake suggestions

--- frontend/android/app/src/main/java/cs/vsu/taskbench/data/task/FakeSuggestionRepository.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/data/task/FakeSuggestionRepository.kt
@@ -1,19 +1,37 @@
 package cs.vsu.taskbench.data.task
 
 import android.util.Log
+import kotlin.random.Random
 
 object FakeSuggestionRepository : SuggestionRepository {
     private val TAG = FakeSuggestionRepository::class.simpleName
 
-    private val SUGGESTIONS = listOf(
-        "Lorem ipsum dolor sit amet",
-        "Maecenas faucibus tellus eget",
-        "Integer vel venenatis arcu",
-    )
+    private val LIPSUM =
+        ("lorem ipsum dolor sit amet consectetur adipiscing elit phasellus dictum sem nec " +
+                "convallis egestas quisque felis ligula imperdiet non porta eget euismod vitae " +
+                "massa nunc feugiat tortor sit amet tempus ultrices elit massa auctor neque a " +
+                "pulvinar elit lacus at orci etiam dictum lacus quis consequat dictum cras " +
+                "vulputate pulvinar enim eget elementum").splitToSequence(" ").distinct().toList()
 
     override suspend fun getSuggestions(prompt: String): List<String> {
         Log.d(TAG, "requested suggestions")
-        Log.d(TAG, "returned ${SUGGESTIONS.size} suggestions")
-        return SUGGESTIONS
+
+        if (prompt.isBlank()) {
+            Log.d(TAG, "empty prompt, returning nothing")
+            return listOf()
+        }
+
+        val random = Random(prompt.hashCode())
+        val result = mutableListOf<String>()
+        repeat(random.nextInt(0, 7)) {
+            val words = mutableListOf<String>()
+            repeat(random.nextInt(4, 9)) {
+                words += LIPSUM.random(random)
+            }
+            result += "${it + 1}. ${words.joinToString(" ")}"
+        }
+
+        Log.d(TAG, "returned ${result.size} suggestions")
+        return result
     }
 }
