d6d1fdf78a15386af4cb749af6f7c04c1ff5cdee
heathlisss <heathlisss@gmail.com>
2025-04-25 12:35:17 +0300

    #6: add the view framework and viewmodel TaskCreationScreen

--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/create/TaskCreationScreen.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/create/TaskCreationScreen.kt
@@ -3,9 +3,10 @@ package cs.vsu.taskbench.ui.create
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,11 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,8 +28,11 @@ import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import cs.vsu.taskbench.R
+import cs.vsu.taskbench.domain.model.Subtask
 import cs.vsu.taskbench.ui.ScreenTransitions
+import cs.vsu.taskbench.ui.component.BoxEdit
 import cs.vsu.taskbench.ui.component.Chip
+import cs.vsu.taskbench.ui.component.CreateSubtaskField
 import cs.vsu.taskbench.ui.theme.AccentYellow
 import cs.vsu.taskbench.ui.theme.Beige
 import cs.vsu.taskbench.ui.theme.Black
@@ -31,109 +40,125 @@ import cs.vsu.taskbench.ui.theme.LightGray
 import cs.vsu.taskbench.ui.theme.TaskbenchTheme
 import cs.vsu.taskbench.ui.theme.White
 
+@Stable
+private data class TaskCreationScreenState(
+    val task: String,
+    val onTaskChange: (String) -> Unit,
+    val newSubtask: String,
+    val onNewSubtaskChange: (String) -> Unit,
+    val priority: String,
+    val deadline: String,
+    val category: String,
+    var subtasks: List<Subtask>,
+    var suggestions: List<Subtask>,
+    val onDeadline: () -> Unit,
+    val onPriority: () -> Unit,
+    val onCategory: () -> Unit,
+    val onSubtaskClick: () -> Unit,
+    val onAddTask: () -> Unit,
+)
+
+
 @Destination<RootGraph>(style = ScreenTransitions::class)
 @Composable
 fun TaskCreationScreen(
+    //    navController: NavController,
 ) {
+
+}
+
+@Composable
+private fun TaskCreationContent(
+    stateScreen: TaskCreationScreenState
+){
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Beige)
-            .padding(8.dp),
-    ) {
-        Column(
-            modifier = Modifier
-                .height(263.dp)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-        }
-
+    ){
         Icon(
             painter = painterResource(R.drawable.logo_full_dark),
             contentDescription = "",
             tint = Color.Unspecified,
-            modifier = Modifier
-                .align(Alignment.Center)
+            modifier = Modifier.align(Alignment.Center)
         )
 
         Column(
             modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 80.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-
+                .fillMaxSize()
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 96.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            Row(
-//                modifier = Modifier
-//                    .horizontalScroll(rememberScrollState()),
-//                horizontalArrangement = Arrangement.spacedBy(8.dp)
-//            ) {
-//                Button(
-//                    "Выбрать время",
-//                    onClick = { Log.d(null, "lorem ipsum clicked") }
-//                )
-//                Button(
-//                    "Категория",
-//                    onClick = { Log.d(null, "lorem ipsum clicked") }
-//                )
-//                Button(
-//                    "Приоритет",
-//                    onClick = { Log.d(null, "lorem ipsum clicked") }
-//                )
-//            }
 
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.horizontalScroll(rememberScrollState()),
+            CreateSubtaskField(
+                text = stateScreen.newSubtask,
+                onTextChange = stateScreen.onNewSubtaskChange,
+                placeholder = "Enter subtask",
+                onAddButtonClick = stateScreen.onSubtaskClick,
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+
             ) {
-                Chip(
-                    "tomorrow, 17:30",
-                    White,
-                    Black,
-                    {},
-                    icon = painterResource(R.drawable.ic_clock)
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier.horizontalScroll(rememberScrollState()),
+                ) {
+                    Chip("date date date", White, Black, {}, icon = painterResource(R.drawable.ic_clock))
+                    Chip("high priority", AccentYellow, Black, {})
+                    Chip("select category", White, LightGray, {})
+                }
+                BoxEdit(
+                    value = stateScreen.task,
+                    onValueChange = stateScreen.onTaskChange,
+                    buttonIcon = painterResource(R.drawable.ic_add_circle_filled),
+                    inactiveButtonIcon = painterResource(R.drawable.ic_add_circle_outline),
+                    placeholder = "Enter text",
                 )
-                Chip("high priority", AccentYellow, Black, {})
-                Chip("select category", White, LightGray, {})
             }
 
-
-//            CardEntryField(
-//                "",
-//                "Введите вашу идею",
-//                {}
-//            )
         }
     }
 }
-//    navController: NavController,
-//) {
-//    Scaffold(
-//        bottomBar = {
-//            Column {
-//                NavigationBar(navController)
-//            }
-//        }
-//    ) { padding ->
-//        // TODO!
-//        Text(
-//            text = "Task Creation Screen",
-//            fontSize = 28.sp,
-//            color = LightGray,
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .wrapContentSize()
-//                .padding(padding),
-//        )
-//}}
 
 
 @Preview
 @Composable
 private fun Preview() {
+    var task by remember { mutableStateOf("")}
+    var newSubtask by remember { mutableStateOf("")}
+    var priority by remember { mutableStateOf("")}
+    var deadline by remember { mutableStateOf("")}
+    var category by remember { mutableStateOf("")}
+    var subtasks: List<Subtask> by remember { mutableStateOf(emptyList())}
+    var suggestions: List<Subtask> by remember { mutableStateOf(emptyList())}
+
+    val screenState = TaskCreationScreenState(
+        task = task,
+        onTaskChange = { task = it },
+        newSubtask = newSubtask,
+        onNewSubtaskChange = { newSubtask = it },
+        priority = priority,
+        deadline = deadline,
+        category = category,
+        subtasks = subtasks,
+        suggestions = suggestions,
+        onDeadline = { },
+        onPriority = { },
+        onCategory = {},
+        onSubtaskClick = {},
+        onAddTask = {},
+    )
+
     TaskbenchTheme {
-        TaskCreationScreen()
+        TaskCreationContent(screenState)
     }
 }
--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/create/TaskCreationScreenViewModel.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/create/TaskCreationScreenViewModel.kt
@@ -1,4 +1,67 @@
 package cs.vsu.taskbench.ui.create
 
-class TaskCreationScreenViewModel {
+import androidx.annotation.StringRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import cs.vsu.taskbench.domain.model.Subtask
+
+class TaskCreationScreenViewModel(
+
+) : ViewModel()  {
+
+    enum class ErrorType(@StringRes val messageId: Int) {
+    }
+
+    sealed interface Event {
+        data class SaveTask(val itemId: String) : Event
+        data class Save(val itemId: String) : Event
+        data class Edit(val itemId: String) : Event
+        data class Delete(val itemId: String) : Event
+        data class Add(val itemId: String) : Event
+        data class Create(val value: String) : Event
+
+    }
+
+    fun updateEvent(event: Event) {
+        when (event) {
+            is Event.SaveTask -> saveTask()
+            is Event.Edit -> TODO()
+            is Event.Save -> TODO()
+            is Event.Add -> addSubtask()
+            is Event.Delete -> deleteSubtask()
+            is Event.Create -> createSubtask()
+        }
+    }
+
+    var task by mutableStateOf("")
+    var newSubtask by mutableStateOf("")
+    var priority by mutableStateOf("")
+    var deadline by mutableStateOf("")
+    var category by mutableStateOf("")
+    var subtasks: List<Subtask> by mutableStateOf(emptyList())
+    var suggestions: List<Subtask> by mutableStateOf(emptyList())
+
+
+    fun addSubtask(){
+
+    }
+
+    fun deleteSubtask(){
+
+    }
+
+    fun saveTask(){
+
+    }
+
+    fun createSubtask(){
+
+    }
+
+
+
+
 }
