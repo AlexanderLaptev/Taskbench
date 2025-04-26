91d20824aa1fe6f73dcbe5a65a036989d32995b1
Soopcha <lena.malykhina.04@gmail.com>
2025-04-26 20:58:08 +0300

    #70: update views and Test.pu

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,16 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from taskbench.views.task_views import task_list, task_detail, subtask_create, subtask_detail
 from taskbench.views.user_views import RegisterView, LoginView, DeleteUserView, TokenRefreshView, ChangePasswordView
+from taskbench.views.task_views import (
+    TaskListView,
+    TaskDetailView,
+    SubtaskCreateView,
+    SubtaskDetailView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('api/tasks/', task_list, name='task_list'),
-    path('tasks/', task_list, name='task_list'),  # Соответствует OpenAPI
-    path('tasks/<int:task_id>/', task_detail, name='task_detail'), # PATCH/DELETE конкретной задачи
-    path('subtasks/', subtask_create, name='subtask_create'), # POST - создание подзадачи
-    path('subtasks/<int:subtask_id>/', subtask_detail, name='subtask_detail'), # PATCH/DELETE подзадачи
+    # Tasks endpoints
+    path('tasks/', TaskListView.as_view(), name='task_list'),
+    path('tasks/<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
+
+    # Subtasks endpoints
+    path('subtasks/', SubtaskCreateView.as_view(), name='subtask_create'),
+    path('subtasks/<int:subtask_id>/', SubtaskDetailView.as_view(), name='subtask_detail'),
+
     path('user/register/', RegisterView.as_view(), name='register'), # POST - создание пользователя
     path('user/login', LoginView.as_view(), name='login'), # POST - валидация пользователя, возвращение jwt
     path('user/delete', DeleteUserView.as_view(), name='delete_user'), # POST - валидация пользователя, возвращение jwt
