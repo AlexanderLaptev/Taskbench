f45771a683fc55cd026a274814528e8672100b9a
imbeer <alekceykocenko@gmail.com>
2025-04-28 17:46:02 +0300

    fix paths in accordance with Open API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,13 +19,6 @@ from django.urls import path
 
 from taskbench.views.statisctics_views import StatisticsView
 from taskbench.views.suggestion_views import SuggestionView
-from taskbench.views.user_views import (
-    RegisterView, 
-    LoginView, 
-    DeleteUserView, 
-    TokenRefreshView, 
-    ChangePasswordView
-)
 from taskbench.views.task_views import (
     TaskListView,
     TaskDetailView,
@@ -33,26 +26,27 @@ from taskbench.views.task_views import (
     SubtaskDetailView,
     CategoryListView
 )
+from taskbench.views.user_views import (
+    RegisterView,
+    LoginView,
+    DeleteUserView,
+    TokenRefreshView,
+    ChangePasswordView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/tasks/', task_list, name='task_list'),
-    # Tasks endpoints
     path('tasks/', TaskListView.as_view(), name='task_list'),
     path('tasks/<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
-
-    # Subtasks endpoints
     path('subtasks/', SubtaskCreateView.as_view(), name='subtask_create'),
     path('subtasks/<int:subtask_id>/', SubtaskDetailView.as_view(), name='subtask_detail'),
-
     path('categories/', CategoryListView.as_view(), name='categories'),
-
     path('user/register/', RegisterView.as_view(), name='register'), # POST - создание пользователя
-    path('user/login', LoginView.as_view(), name='login'), # POST - валидация пользователя, возвращение jwt
-    path('user/delete', DeleteUserView.as_view(), name='delete_user'), # POST - валидация пользователя, возвращение jwt
-    path('user/password', ChangePasswordView.as_view(), name='change_password'),
+    path('user/login/', LoginView.as_view(), name='login'), # POST - валидация пользователя, возвращение jwt
+    path('user/delete/', DeleteUserView.as_view(), name='delete_user'), # POST - валидация пользователя, возвращение jwt
+    path('user/password/', ChangePasswordView.as_view(), name='change_password'),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('statistics', StatisticsView.as_view(), name='statistics'),
-    path("ai/suggestions", SuggestionView.as_view(), name="ai_suggestions"),
+    path("ai/suggestions/", SuggestionView.as_view(), name="ai_suggestions"),
 ]
 
