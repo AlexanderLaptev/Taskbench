85657ed25b58bff3fe7d53062cefdf5de0783afb
imbeer <alekceykocenko@gmail.com>
2025-04-30 23:15:14 +0300

    #24: add subscriptions page and base for api

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from dashboard.views import custom_login, dashboard_view
+from dashboard.views import custom_login, dashboard_view, stats_api, subscription_list_api, subscription_page
 
 urlpatterns = [
     path("admin/login/", custom_login, name="custom_login"),
     path("admin/dashboard/", dashboard_view, name="dashboard"),
+    path("admin/subscriptions/", subscription_page, name="admin_subscriptions"),
+    path("admin/api/stats/", stats_api, name="stats_api"),
+    path("admin/api/subscriptions/", subscription_list_api, name="subscription_list_api"),
 ]
