6a106195149663a88e85c46b9074eb240ab7058f
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-02 04:02:51 +0300

    #41: add a mock-up for the date row

--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/list/TaskListScreen.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/list/TaskListScreen.kt
@@ -1,13 +1,19 @@
 package cs.vsu.taskbench.ui.list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Scaffold
@@ -18,9 +24,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
@@ -30,6 +39,7 @@ import cs.vsu.taskbench.ui.ScreenTransitions
 import cs.vsu.taskbench.ui.component.DropdownOptions
 import cs.vsu.taskbench.ui.component.NavigationBar
 import cs.vsu.taskbench.ui.component.TaskCard
+import cs.vsu.taskbench.ui.theme.AccentYellow
 import cs.vsu.taskbench.ui.theme.White
 import org.koin.androidx.compose.koinViewModel
 import java.time.LocalDateTime
@@ -63,6 +73,7 @@ fun TaskListScreen(
             modifier = Modifier.padding(padding),
         ) {
             SortModeRow(Modifier.padding(horizontal = 16.dp))
+            DateRow()
 
             LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -149,13 +160,66 @@ private fun RowScope.SortButton(
 }
 
 @Composable
-private fun DateRow(modifier: Modifier = Modifier) {
+private fun DateRow() {
+    val listState = rememberLazyListState(Int.MAX_VALUE / 2)
+    LazyRow(
+        state = listState,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+//        modifier = Modifier.height(80.dp),
+    ) {
+        items(Int.MAX_VALUE) {
+            DateTile(
+                topLabel = "ab",
+                middleLabel = (it % 100).toString(),
+                bottomLabel = "cd",
+                selected = it % 2 == 0,
+                onClick = {},
+            )
+        }
+    }
+}
+
+private val DATE_TILE_SHAPE = RoundedCornerShape(10.dp)
+
+@Composable
+private fun DateTile(
+    topLabel: String,
+    middleLabel: String,
+    bottomLabel: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .clip(DATE_TILE_SHAPE)
+            .clickable(onClick = onClick)
+            .background(
+                color = if (selected) AccentYellow else White,
+                shape = DATE_TILE_SHAPE,
+            )
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .width(40.dp),
+    ) {
+        Text(
+            text = topLabel,
+            fontSize = 16.sp,
+        )
+        Text(
+            text = middleLabel,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = bottomLabel,
+            fontSize = 16.sp,
+        )
+    }
 }
 
 @Composable
 private fun formatDeadline(deadline: LocalDateTime?): String {
     val pattern = stringResource(R.string.pattern_deadline)
     val formatter = remember { DateTimeFormatter.ofPattern(pattern) }
-
     return deadline?.let { formatter.format(deadline) } ?: ""
 }
