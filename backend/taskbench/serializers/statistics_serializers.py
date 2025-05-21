bf9abf0666542102914babea44d89b25f7744824
Soopcha <lena.malykhina.04@gmail.com>
2025-05-22 01:37:40 +0300

    #83: refactor statistics

--- backend/taskbench/serializers/statistics_serializers.py
+++ backend/taskbench/serializers/statistics_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.http import JsonResponse
 
 class StatisticsSerializer(serializers.Serializer):
     done_today = serializers.IntegerField()
@@ -9,3 +9,9 @@ class StatisticsSerializer(serializers.Serializer):
         min_length=7,
         max_length=7
     )
+
+def statistics_response(statistics, status=200):
+    serializer = StatisticsSerializer(data=statistics)  # Передаем данные через data
+    if not serializer.is_valid():
+        raise serializers.ValidationError(serializer.errors)
+    return JsonResponse(serializer.data, status=status)
