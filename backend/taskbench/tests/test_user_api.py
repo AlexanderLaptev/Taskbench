21d478c3cfeeb779b7daedff4ebbb0276e951632
imbeer <alekceykocenko@gmail.com>
2025-05-28 14:12:54 +0300

    #73: clean up previous fake subscription API

--- backend/taskbench/tests/test_user_api.py
+++ backend/taskbench/tests/test_user_api.py
@@ -20,46 +20,48 @@ class UserApiTests(TestCase):
 
     def test_login_and_password_change(self):
         url = reverse('login')
-        response = self.client.post(url,
-                         data={
-                             "email": "example@mail.com",
-                             "password": "test_password"
-                         }, format='json')
+        response = self.client.post(
+            url,
+            data={
+                "email": "example@mail.com",
+                "password": "test_password"
+            }, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsNotNone(response.json().get('access'))
         self.assertIsNotNone(response.json().get('refresh'))
 
         access = str(response.json().get('access'))
         url = reverse('change_password')
-        response = self.client.patch(url,
-                                     data={
-                                         "old_password": "test_password",
-                                         "new_password": "new_password"
-                                     },
-                                     HTTP_AUTHORIZATION=f'Bearer {access}',
-                                     format='json')
+        response = self.client.patch(
+            url,
+            data={
+                "old_password": "test_password",
+                "new_password": "new_password"
+            },
+            HTTP_AUTHORIZATION=f'Bearer {access}',
+            format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         url = reverse('login')
-        response = self.client.post(url,
-                                    data={
-                                        "email": "example@mail.com",
-                                        "password": "new_password"
-                                    },
-                                    format='json')
+        response = self.client.post(
+            url,
+            data={
+                "email": "example@mail.com",
+                "password": "new_password"
+            },
+            format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsNotNone(response.json().get('access'))
         self.assertIsNotNone(response.json().get('refresh'))
 
-
-
     def test_register_refresh_and_delete(self):
         url = reverse('register')
-        response = self.client.post(url,
-                                    data={
-                                        "email": 'new_email@mail.com',
-                                        "password": "test_password"
-                                    }, format='json')
+        response = self.client.post(
+            url,
+            data={
+                "email": 'new_email@mail.com',
+                "password": "test_password"
+            }, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(response.json().get('access'))
         self.assertIsNotNone(response.json().get('refresh'))
--- backend/taskbench/views/user_views.py
+++ backend/taskbench/views/user_views.py
@@ -1,15 +1,11 @@
 import json
-import uuid
-from datetime import timedelta
 
 from django.http import JsonResponse
-from django.utils import timezone
 from pydantic import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models.models import Subscription
-from ..serializers.user_serializers import JwtSerializer, user_response
+from ..serializers.user_serializers import user_response
 from ..services.user_service import get_token, register_user, login_user, token_refresh, delete_user, change_password, \
     AuthenticationError
 
@@ -17,8 +13,8 @@ from ..services.user_service import get_token, register_user, login_user, token_
 class RegisterView(APIView):
 
     def post(self, request, *args, **kwargs):
-        data = json.loads(request.body)
         try:
+            data = json.loads(request.body)
             return user_response(*register_user(data), status=201)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
@@ -27,10 +23,11 @@ class RegisterView(APIView):
 class LoginView(APIView):
 
     def post(self, request, *args, **kwargs):
-        data = json.loads(request.body)
         try:
+            data = json.loads(request.body)
             return user_response(*login_user(data), status=200)
         except Exception as e:
+            print(str(e))
             return JsonResponse({'error': str(e)}, status=400)
 
 
@@ -48,8 +45,8 @@ class DeleteUserView(APIView):
 class ChangePasswordView(APIView):
 
     def patch(self, request, *args, **kwargs):
-        data = json.loads(request.body)
         try:
+            data = json.loads(request.body)
             change_password(token=get_token(request), data=data)
             return Response(status=204)
         except AuthenticationError as e:
@@ -61,72 +58,11 @@ class ChangePasswordView(APIView):
 class TokenRefreshView(APIView):
 
     def post(self, request, *args, **kwargs):
-        data = json.loads(request.body)
-        token = {'token': str(data['refresh'])}
-        if data['refresh'] is None:
-            return Response('No token provided.', status=400)
         try:
+            data = json.loads(request.body)
+            token = {'token': str(data['refresh'])}
+            if data['refresh'] is None:
+                return Response('No token provided.', status=400)
             return user_response(*token_refresh(token), status=200)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
-
-
-# class SubscriptionStatusView(APIView):
-#     def get(self, request):
-#         token = get_token(request)
-#         serializer = JwtSerializer(data=token)
-#         if not serializer.is_valid():
-#             return JsonResponse({'error': 'Invalid token'}, status=401)
-#         user = serializer.validated_data['user']
-#
-#         # Проверяем активную подписку
-#         now = timezone.now()
-#         active_subscription = user.subscriptions.filter(
-#             is_active=True,
-#             start_date__lte=now,
-#             end_date__gte=now
-#         ).first()
-#
-#         if active_subscription:
-#             return JsonResponse({
-#                 'has_subscription': True,
-#                 'start_date': active_subscription.start_date,
-#                 'end_date': active_subscription.end_date,
-#                 'transaction_id': active_subscription.transaction_id
-#             })
-#         else:
-#             return JsonResponse({
-#                 'has_subscription': False
-#             })
-#
-#
-# class CreateSubscriptionView(APIView):
-#     def post(self, request):
-#         # Проверка JWT токена
-#         token = get_token(request)
-#         serializer = JwtSerializer(data=token)
-#         if not serializer.is_valid():
-#             return JsonResponse({'error': 'Invalid token'}, status=401)
-#         user = serializer.validated_data['user']
-#
-#         now = timezone.now()
-#         end_date = now + timedelta(days=30)
-#
-#         if user.subscriptions.filter(is_active=True, end_date__gte=now).exists():
-#             return JsonResponse({'error': 'User already has active subscription'}, status=400)
-#
-#         subscription = Subscription.objects.create(
-#             user=user,
-#             start_date=now,
-#             end_date=end_date,
-#             is_active=True,
-#             transaction_id=str(uuid.uuid4())
-#         )
-#
-#         return JsonResponse({
-#             'status': 'success',
-#             'subscription_id': subscription.subscription_id,
-#             'start_date': subscription.start_date,
-#             'end_date': subscription.end_date,
-#             'transaction_id': subscription.transaction_id
-#         }, status=201)
