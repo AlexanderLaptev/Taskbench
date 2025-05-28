ff413f52c2fd3b770930ba98d82b8aec3583e4bb
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-28 18:10:44 +0300

    redesign the statistics screen

--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/StatisticsScreen.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/StatisticsScreen.kt
@@ -4,45 +4,65 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
-import com.ramcosta.composedestinations.generated.destinations.PremiumManagementScreenDestination
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import cs.vsu.taskbench.R
 import cs.vsu.taskbench.data.analytics.AnalyticsFacade
 import cs.vsu.taskbench.data.statistics.StatisticsRepository
-import cs.vsu.taskbench.data.user.UserRepository
 import cs.vsu.taskbench.domain.model.Statistics
 import cs.vsu.taskbench.domain.model.User
+import cs.vsu.taskbench.ui.component.Button
 import cs.vsu.taskbench.ui.component.NavigationBar
-import cs.vsu.taskbench.ui.component.TitleDailyStats
 import cs.vsu.taskbench.ui.component.WeekStatistics
-import cs.vsu.taskbench.ui.settings.WithPremium
-import cs.vsu.taskbench.ui.settings.WithoutPremium
+import cs.vsu.taskbench.ui.theme.AccentYellow
+import cs.vsu.taskbench.ui.theme.DarkGray
+import cs.vsu.taskbench.ui.theme.TaskbenchTheme
 import cs.vsu.taskbench.ui.theme.White
 import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 import java.net.ConnectException
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 private const val TAG = "StatisticsScreen"
 
@@ -54,8 +74,6 @@ fun StatisticsScreen(
     val snackbarHostState = remember { SnackbarHostState() }
     val statisticsRepository = koinInject<StatisticsRepository>()
     var statistics by remember { mutableStateOf<Statistics?>(null) }
-    val userRepo = koinInject<UserRepository>()
-    val user = userRepo.user!!
     val destinationsNavigator = navController.rememberDestinationsNavigator()
 
     LaunchedEffect(Unit) {
@@ -82,62 +100,227 @@ fun StatisticsScreen(
             }
         }
 
-        Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+        if (statistics == null) return@Scaffold
+        @Suppress("NAME_SHADOWING") val statistics = statistics!!
+        val graphData = FloatArray(7) { statistics.graphData[it] }
+
+        Content(
+            graphLevels = graphData,
+            doneToday = statistics.doneToday,
+            allTimeHigh = statistics.doneAllTimeHigh,
+            userStatus = User.Status.Unpaid,
             modifier = Modifier
-                .fillMaxWidth()
                 .padding(16.dp)
                 .padding(padding),
+        )
+    }
+}
+
+@Composable
+private fun Content(
+    graphLevels: FloatArray,
+    doneToday: Int,
+    allTimeHigh: Int,
+    userStatus: User.Status,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.verticalScroll(rememberScrollState()),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = White, shape = RoundedCornerShape(10.dp))
+                .padding(horizontal = 16.dp),
         ) {
-            if (statistics == null) return@Scaffold
-            @Suppress("NAME_SHADOWING") val statistics = statistics!!
-            val graphData = FloatArray(7) { statistics.graphData[it] }
-
-            WeekStatistics(
-                levels = graphData,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f)
-                    .background(color = White, shape = RoundedCornerShape(10.dp))
-                    .padding(16.dp),
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.label_week_statistics),
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                modifier = Modifier.fillMaxWidth(),
             )
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                TitleDailyStats(
-                    text = stringResource(R.string.label_done_today),
-                    dailyStats = statistics.doneToday,
-                    modifier = Modifier.weight(1f),
+            WeekStatistics(
+                levels = graphLevels,
+            )
+            if (graphLevels.all { it > 0.5f }) {
+                Spacer(Modifier.height(6.dp))
+                Text(
+                    text = stringResource(R.string.label_good_job),
+                    color = DarkGray,
+                    fontSize = 16.sp,
+                    fontStyle = FontStyle.Italic,
                 )
-                TitleDailyStats(
-                    text = stringResource(R.string.label_done_all_time_high),
-                    dailyStats = statistics.doneAllTimeHigh,
-                    modifier = Modifier.weight(1f),
-                )
-            }
-            when (user.status) {
-                is User.Status.Premium -> {
-                    WithPremium(
-                        user.status.activeUntil,
-                        onClick = {
-                            destinationsNavigator.navigate(
-                                PremiumManagementScreenDestination
-                            )
-                        }
-                    )
-                }
-
-                User.Status.Unpaid -> {
-                    WithoutPremium(
-                        onClick = {
-                            destinationsNavigator.navigate(
-                                PremiumManagementScreenDestination
-                            )
-                        }
-                    )
-                }
+                Spacer(Modifier.height(8.dp))
+            } else {
+                Spacer(Modifier.height(16.dp))
             }
         }
 
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.height(IntrinsicSize.Max),
+        ) {
+            StatsTile(
+                title = stringResource(R.string.label_done_today),
+                value = doneToday.toString(),
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight(),
+            )
+            StatsTile(
+                title = stringResource(R.string.label_done_all_time_high),
+                value = allTimeHigh.toString(),
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight(),
+            )
+        }
+
+        when (userStatus) {
+            User.Status.Unpaid -> WhenUnpaid()
+            is User.Status.Premium -> WhenPaid(userStatus)
+        }
+    }
+}
+
+@Composable
+private fun StatsTile(
+    title: String,
+    value: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .background(color = White, shape = RoundedCornerShape(10.dp))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(),
+        ) {
+            val density = LocalDensity.current
+            Text(
+                text = value,
+                color = DarkGray,
+                fontSize = with(density) { 64.dp.toSp() },
+                modifier = Modifier
+            )
+            Text(
+                text = stringResource(R.string.label_stats_tile_tasks),
+                color = DarkGray,
+                fontSize = 20.sp,
+                modifier = Modifier.offset(y = (-8).dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun WhenUnpaid(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(color = AccentYellow, shape = RoundedCornerShape(10.dp))
+            .padding(16.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.text_stats_premium_part1),
+            textAlign = TextAlign.Center,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = buildAnnotatedString {
+                append(stringResource(R.string.text_stats_premium_part2))
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Medium)) {
+                    append(stringResource(R.string.text_stats_premium_part3))
+                }
+                append(stringResource(R.string.text_stats_premium_part4))
+            },
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.height(8.dp))
+        Button(
+            text = stringResource(R.string.button_stats_premium),
+            onClick = {},
+            color = White,
+        )
+    }
+}
+
+@Composable
+fun WhenPaid(userStatus: User.Status.Premium, modifier: Modifier = Modifier) {
+    val pattern = stringResource(R.string.pattern_date)
+    val dateFormatter = remember { DateTimeFormatter.ofPattern(pattern) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(color = White, shape = RoundedCornerShape(10.dp))
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.label_premium_active),
+            textAlign = TextAlign.Center,
+            fontSize = 18.sp,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Text(
+            text = stringResource(
+                R.string.label_premium_until,
+                dateFormatter.format(userStatus.activeUntil)
+            ),
+            color = DarkGray,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+private val previewLevels = floatArrayOf(0.83f, 0.59f, 0.76f, 1.0f, 0.57f, 0.91f, 0.73f)
+private const val PREVIEW_DONE_TODAY = 18
+private const val PREVIEW_DONE_MAX = 29
+
+@Preview
+@Composable
+private fun PreviewUnpaid() {
+    TaskbenchTheme {
+        Content(
+            graphLevels = previewLevels,
+            doneToday = PREVIEW_DONE_TODAY,
+            allTimeHigh = PREVIEW_DONE_MAX,
+            userStatus = User.Status.Unpaid,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewPremium() {
+    TaskbenchTheme {
+        Content(
+            graphLevels = previewLevels,
+            doneToday = PREVIEW_DONE_TODAY,
+            allTimeHigh = PREVIEW_DONE_MAX,
+            userStatus = User.Status.Premium(LocalDate.of(2025, 5, 30)),
+        )
     }
 }
