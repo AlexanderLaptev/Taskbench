140ba9d7a3198a88a8108c549b3c46a114463567
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-31 00:05:54 +0300

    remove the "forgot password?" option from the login screen

--- frontend/android/app/src/main/java/cs/vsu/taskbench/ui/login/LoginScreen.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/ui/login/LoginScreen.kt
@@ -5,9 +5,6 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -47,7 +44,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -64,8 +60,6 @@ import cs.vsu.taskbench.ui.login.LoginScreenViewModel.Event.Error
 import cs.vsu.taskbench.ui.theme.Beige
 import cs.vsu.taskbench.ui.theme.Black
 import cs.vsu.taskbench.ui.theme.LightYellow
-import cs.vsu.taskbench.ui.theme.Link
-import cs.vsu.taskbench.ui.theme.LinkPressed
 import cs.vsu.taskbench.ui.theme.White
 import cs.vsu.taskbench.ui.theme.swapTransition
 import kotlinx.coroutines.launch
@@ -295,31 +289,32 @@ private fun LoginControls(
             White,
         )
 
-        AnimatedVisibility(
-            visible = screenState.loginState == LoginState.Login,
-        ) {
-            val interactionSource = remember { MutableInteractionSource() }
-            val isLinkPressed by interactionSource.collectIsPressedAsState()
-
-            Column {
-                Spacer(Modifier.height(LOGIN_CONTROLS_ITEM_SPACING))
-                Text(
-                    text = stringResource(R.string.label_forgot_password),
-                    fontSize = 14.sp,
-                    color = if (isLinkPressed) LinkPressed else Link,
-                    textDecoration = TextDecoration.Underline,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .clickable(
-                            onClick = screenState.onForgotPassword,
-                            interactionSource = interactionSource,
-                            indication = null,
-                        )
-                        .height(32.dp)
-                        .fillMaxWidth(0.5f),
-                )
-            }
-        }
+        // TODO! Taking this out temporarily 'cause we can't send emails (still)
+//        AnimatedVisibility(
+//            visible = screenState.loginState == LoginState.Login,
+//        ) {
+//            val interactionSource = remember { MutableInteractionSource() }
+//            val isLinkPressed by interactionSource.collectIsPressedAsState()
+//
+//            Column {
+//                Spacer(Modifier.height(LOGIN_CONTROLS_ITEM_SPACING))
+//                Text(
+//                    text = stringResource(R.string.label_forgot_password),
+//                    fontSize = 14.sp,
+//                    color = if (isLinkPressed) LinkPressed else Link,
+//                    textDecoration = TextDecoration.Underline,
+//                    textAlign = TextAlign.Center,
+//                    modifier = Modifier
+//                        .clickable(
+//                            onClick = screenState.onForgotPassword,
+//                            interactionSource = interactionSource,
+//                            indication = null,
+//                        )
+//                        .height(32.dp)
+//                        .fillMaxWidth(0.5f),
+//                )
+//            }
+//        }
     }
 }
 
