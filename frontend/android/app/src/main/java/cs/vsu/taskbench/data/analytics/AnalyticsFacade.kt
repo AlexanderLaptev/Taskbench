4d1a8be31046f1d2e5f07884b69fc187815ebc74
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-06-02 23:33:51 +0300

    add logging to analytics

--- frontend/android/app/src/main/java/cs/vsu/taskbench/data/analytics/AnalyticsFacade.kt
+++ frontend/android/app/src/main/java/cs/vsu/taskbench/data/analytics/AnalyticsFacade.kt
@@ -1,9 +1,12 @@
 package cs.vsu.taskbench.data.analytics
 
+import android.util.Log
 import cs.vsu.taskbench.BuildConfig
 import io.appmetrica.analytics.AppMetrica
 
 object AnalyticsFacade {
+    private val TAG = AnalyticsFacade::class.simpleName
+
     fun logScreen(screenName: String) {
         reportEvent("screen_view", mapOf("screen_name" to screenName))
     }
@@ -20,16 +23,19 @@ object AnalyticsFacade {
     private fun reportEvent(eventName: String, attributes: Map<String, *>) {
         if (BuildConfig.DEBUG) return
         AppMetrica.reportEvent(eventName, attributes)
+        Log.d(TAG, "reportEvent: $eventName {${attributes.entries.joinToString()}}")
     }
 
     @Suppress("SameParameterValue")
     private fun reportEvent(eventName: String) {
         if (BuildConfig.DEBUG) return
         AppMetrica.reportEvent(eventName)
+        Log.d(TAG, "reportEvent: $eventName")
     }
 
     private fun reportError(tag: String, throwable: Throwable) {
         if (BuildConfig.DEBUG) return
         AppMetrica.reportError(tag, throwable)
+        Log.e(TAG, "reportError: $tag")
     }
 }
